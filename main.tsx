98546bccec7bd8b1853e8dbc7fd9dddc246f52cf
Kota Kato <45391880+kat0h@users.noreply.github.com>
2023-11-24 13:59:19 +0900

    New file

---
+++ main.tsx
@@ -0,0 +1,81 @@
+/** @jsx jsx */
+/** @jsxFrag Fragment */
+
+// PCでuuid付きの使い捨てのURLを生成→QRに
+// スマホからアップロード
+// PCの画面にはBASIC認証を書ける
+//
+
+import { Hono } from "https://deno.land/x/hono@v3.10.2/mod.ts";
+import {
+  Fragment,
+  jsx,
+  serveStatic,
+} from "https://deno.land/x/hono@v3.10.2/middleware.ts";
+import { logger } from "https://deno.land/x/hono@v3.10.2/middleware.ts";
+
+const app = new Hono();
+let sock: undefined | WebSocket = undefined;
+
+app.use("*", logger());
+app.get("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
+
+app.get("/upload", (c) => {
+  return c.html(
+    <>
+      <html>
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </head>
+        <body>
+          <p1>アップローダー</p1>
+          <form action="/upload" method="POST" enctype="multipart/form-data">
+            <input type="file" name="image" accept="image/jpeg"/>
+            <button type="submit">送信</button>
+          </form>
+        </body>
+      </html>
+    </>,
+  );
+});
+app.post("/upload", async (c) => {
+  console.log(await c.req.parseBody())
+  const { image } = await c.req.parseBody();
+  console.log(image)
+  if (sock !== undefined && image instanceof File)
+    sock.send(image);
+  return c.redirect("/upload");
+});
+
+app.get("/pc", (c) => {
+  // https://cdn.tailwindcss.com
+  return c.html(
+    <>
+      <html>
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <script src="/pc/js"></script>
+        </head>
+        <body>
+          <p1>画像取得</p1>
+          <div id="main">
+          </div>
+        </body>
+      </html>
+    </>,
+  );
+});
+app.get("/pc/js", serveStatic({ path: "./pc.js" }));
+app.get("/pc/ws", (c) => {
+  const { response, socket } = Deno.upgradeWebSocket(c.req.raw);
+  sock = socket;
+  return response;
+});
+
+Deno.serve(app.fetch);
---
+++ pc.js
@@ -0,0 +1,19 @@
+console.log("pc");
+
+const url = new URL(window.location.href);
+url.protocol = "ws";
+url.pathname = "/pc/ws";
+ws = new WebSocket(url);
+
+ws.addEventListener("message", (m) => {
+  console.log("Image");
+  const image = URL.createObjectURL(m.data);
+  const main = document.getElementById("main");
+
+  const elem = document.createElement("img");
+  elem.src = image;
+  elem.width = 300;
+  elem.onload = () => URL.revokeObjectURL(image)
+
+  main.prepend(elem);
+});
